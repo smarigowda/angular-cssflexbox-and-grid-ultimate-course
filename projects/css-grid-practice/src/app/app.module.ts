91ba74d12b623e867fe44946aebe1edc95f785f0
Santosh Marigowda <santosharakere@gmail.com>
2020-04-16 13:15:48 +0100

    resource added and a new component created to demo the basics of grid

--- projects/css-grid-practice/src/app/app.module.ts
+++ projects/css-grid-practice/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GridBasicComponent } from './grid-basic/grid-basic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridBasicComponent
   ],
   imports: [
     BrowserModule,
---
+++ projects/css-grid-practice/src/app/grid-basic/grid-basic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-basic',
+  templateUrl: './grid-basic.component.html',
+  styleUrls: ['./grid-basic.component.scss']
+})
+export class GridBasicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
