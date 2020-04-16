3209ecd708792b63ca392af853e97c9ca408d04d
Santosh Marigowda <santosharakere@gmail.com>
2020-04-16 11:40:07 +0100

    media object layout complete

--- projects/flex-layouts/src/app/app.module.ts
+++ projects/flex-layouts/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ObjectLayoutComponent } from './object-layout/object-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ObjectLayoutComponent
   ],
   imports: [
     BrowserModule,
---
+++ projects/flex-layouts/src/app/object-layout/object-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'media-object-layout',
+  templateUrl: './object-layout.component.html',
+  styleUrls: ['./object-layout.component.scss']
+})
+export class ObjectLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
