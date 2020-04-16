a0b1d718374c35c76d4cf58bffbc9d109e8fe825
Santosh Marigowda <santosharakere@gmail.com>
2020-04-16 11:58:53 +0100

    fixed foot layout css complete

--- projects/flex-layouts/src/app/app.module.ts
+++ projects/flex-layouts/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ObjectLayoutComponent } from './object-layout/object-layout.component';
+import { FixedFootLayoutComponent } from './fixed-foot-layout/fixed-foot-layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ObjectLayoutComponent
+    ObjectLayoutComponent,
+    FixedFootLayoutComponent
   ],
   imports: [
     BrowserModule,
---
+++ projects/flex-layouts/src/app/fixed-foot-layout/fixed-foot-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fixed-foot-layout',
+  templateUrl: './fixed-foot-layout.component.html',
+  styleUrls: ['./fixed-foot-layout.component.scss']
+})
+export class FixedFootLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
