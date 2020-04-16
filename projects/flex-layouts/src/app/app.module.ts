d4f054ea30453a3ad90209dd6a409e5b08970c93
Santosh Marigowda <santosharakere@gmail.com>
2020-04-16 12:23:22 +0100

    css for positions layout complete

--- projects/flex-layouts/src/app/app.module.ts
+++ projects/flex-layouts/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ObjectLayoutComponent } from './object-layout/object-layout.component';
 import { FixedFootLayoutComponent } from './fixed-foot-layout/fixed-foot-layout.component';
+import { PositionsLayoutComponent } from './positions-layout/positions-layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ObjectLayoutComponent,
-    FixedFootLayoutComponent
+    FixedFootLayoutComponent,
+    PositionsLayoutComponent
   ],
   imports: [
     BrowserModule,
---
+++ projects/flex-layouts/src/app/positions-layout/positions-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-positions-layout',
+  templateUrl: './positions-layout.component.html',
+  styleUrls: ['./positions-layout.component.scss']
+})
+export class PositionsLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
