ae0cee09d9658aa9d238f8d6aee47c6776e15fa2
Santosh Marigowda <santosharakere@gmail.com>
2020-04-16 22:13:24 +0100

    a new component added to demo autofill, autofit and minmax

--- projects/css-grid-practice/src/app/app.module.ts
+++ projects/css-grid-practice/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { GridBasicComponent } from './grid-basic/grid-basic.component';
 import { GridTemplateAreaComponent } from './grid-template-area/grid-template-area.component';
 import { GridExperimentalLayoutComponent } from './grid-experimental-layout/grid-experimental-layout.component';
+import { AutofillAutofitMinmaxComponent } from './autofill-autofit-minmax/autofill-autofit-minmax.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GridBasicComponent,
     GridTemplateAreaComponent,
-    GridExperimentalLayoutComponent
+    GridExperimentalLayoutComponent,
+    AutofillAutofitMinmaxComponent
   ],
   imports: [
     BrowserModule,
---
+++ projects/css-grid-practice/src/app/autofill-autofit-minmax/autofill-autofit-minmax.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-autofill-autofit-minmax',
+  templateUrl: './autofill-autofit-minmax.component.html',
+  styleUrls: ['./autofill-autofit-minmax.component.scss']
+})
+export class AutofillAutofitMinmaxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
