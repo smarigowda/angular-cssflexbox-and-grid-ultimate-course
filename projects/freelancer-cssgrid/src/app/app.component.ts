b00e00ce6390e6c9ca11333ca3635c668b21d842
Santosh Marigowda <santosharakere@gmail.com>
2020-04-18 08:07:58 +0100

    populated portfolio content using data

--- projects/freelancer-cssgrid/src/app/app.component.ts
+++ projects/freelancer-cssgrid/src/app/app.component.ts
@@ -6,5 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'freelancer-cssgrid';
+  portfolio;
+  constructor() {
+    this.portfolio = [
+      {
+        id: 1,
+        projectname: "WebSite",
+        desc: "Molestias quam iure quia. Magni accusamus, itaque alias ipsa."
+      },
+      {
+        id: 2,
+        projectname: "App",
+        desc: "Molestias quam iure quia. Magni accusamus, itaque alias ipsa."
+      },
+      {
+        id: 3,
+        projectname: "App",
+        desc: "Pellentesque quis mi et sem faucibus feugiat quis et dui. Pellentesque consectetur magna vitae risus rhoncus volutpat non quis ipsum. Fusce eu maximus turpis, id rutrum dolor. Vivamus est ex, malesuada at leo vitae, eleifend rhoncus velit."
+      },
+      {
+        id: 4,
+        projectname: "Web App",
+        desc: "Molestias quam iure quia. Magni accusamus, itaque alias ipsa."
+      },
+      {
+        id: 5,
+        projectname: "App",
+        desc: "Molestias quam iure quia. Magni accusamus, itaque alias ipsa."
+      },
+      {
+        id: 6,
+        projectname: "App",
+        desc: "Molestias quam iure quia. Magni accusamus, itaque alias ipsa."
+      },
+      {
+        id: 7,
+        projectname: "App",
+        desc: "Molestias quam iure quia. Magni accusamus, itaque alias ipsa."
+      },
+      {
+        id: 8,
+        projectname: "App",
+        desc: "Molestias quam iure quia. Magni accusamus, itaque alias ipsa."
+      }
+    ]
+  }
+
 }
