6c3ef92026704767229813e9ebbd8f33b28a6967
Santosh Marigowda <santosharakere@gmail.com>
2020-04-19 14:37:49 +0100

    demo success

--- projects/image-slider-demo/src/app/app.component.ts
+++ projects/image-slider-demo/src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'image-slider-demo';
+  imageObject: Array<object> = [{
+    image: 'https://i.picsum.photos/id/580/1020/600.jpg',
+    thumbImage: 'https://i.picsum.photos/id/580/400/350.jpg',
+    alt: 'alt of image',
+    title: ''
+  }, {
+    image: 'https://i.picsum.photos/id/838/1020/600.jpg',
+    thumbImage: 'https://i.picsum.photos/id/838/400/350.jpg',
+    title: '',
+    alt: ''
+  }, {
+    image: 'https://i.picsum.photos/id/93/1020/600.jpg',
+    thumbImage: 'https://i.picsum.photos/id/93/400/350.jpg',
+    title: '',
+    alt: ''
+  }, {
+    image: 'https://i.picsum.photos/id/543/1020/600.jpg',
+    thumbImage: 'https://i.picsum.photos/id/543/400/350.jpg',
+    title: '',
+    alt: ''
+  }, {
+    image: 'https://i.picsum.photos/id/613/1020/600.jpg',
+    thumbImage: 'https://i.picsum.photos/id/613/400/350.jpg',
+    title: '',
+    alt: ''
+  }, {
+    image: 'https://i.picsum.photos/id/609/1020/600.jpg',
+    thumbImage: 'https://i.picsum.photos/id/609/400/350.jpg',
+    title: '',
+    alt: ''
+  }, {
+    image: 'https://i.picsum.photos/id/717/1020/600.jpg',
+    thumbImage: 'https://i.picsum.photos/id/717/400/350.jpg',
+    title: '',
+    alt: ''
+  }
+  ];
 }
