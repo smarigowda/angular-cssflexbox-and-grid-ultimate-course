2d1ba376950a57fa35bebde82960fc0334dad164
Santosh Marigowda <santosharakere@gmail.com>
2020-04-19 20:34:49 +0100

    a new component to demo image slider using cycle 2

--- projects/cycle2-demo/src/app/app.module.ts
+++ projects/cycle2-demo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlideshowImagesComponent } from './slideshow-images/slideshow-images.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlideshowImagesComponent
   ],
   imports: [
     BrowserModule,
---
+++ projects/cycle2-demo/src/app/slideshow-images/slideshow-images.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow-images',
+  templateUrl: './slideshow-images.component.html',
+  styleUrls: ['./slideshow-images.component.scss']
+})
+export class SlideshowImagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
