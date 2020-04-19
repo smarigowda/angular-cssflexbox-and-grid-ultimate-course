1b7ecc99e28c3294dc5f37856f5ce9d9ebd176ec
Santosh Marigowda <santosharakere@gmail.com>
2020-04-19 20:45:24 +0100

    a new component to demo slider with non images

--- projects/cycle2-demo/src/app/app.module.ts
+++ projects/cycle2-demo/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SlideshowImagesComponent } from './slideshow-images/slideshow-images.component';
+import { SlideshowNonImagesComponent } from './slideshow-non-images/slideshow-non-images.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SlideshowImagesComponent
+    SlideshowImagesComponent,
+    SlideshowNonImagesComponent
   ],
   imports: [
     BrowserModule,
---
+++ projects/cycle2-demo/src/app/slideshow-non-images/slideshow-non-images.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow-non-images',
+  templateUrl: './slideshow-non-images.component.html',
+  styleUrls: ['./slideshow-non-images.component.scss']
+})
+export class SlideshowNonImagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
