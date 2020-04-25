3eee94dd6d9ff852078633f120d690d5fb277111
Santosh Marigowda <santosharakere@gmail.com>
2020-04-25 22:08:27 +0100

    demo success

--- projects/magnific-popup-demo/src/app/magnific-popup/magnific-popup.component.ts
+++ projects/magnific-popup-demo/src/app/magnific-popup/magnific-popup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-magnific-popup',
@@ -10,6 +11,25 @@ export class MagnificPopupComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log($('.popup-gallery'));
+    // $(document).ready(function () {
+    $('.popup-gallery').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+      },
+      image: {
+        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+        titleSrc: function (item) {
+          return item.el.attr('title') + '<small>Pappadams Gallery</small>';
+        }
+      }
+    });
+    // });
   }
-
 }
