4b7f1bf89c945abb3a8d077383382da58bcf8603
Santosh Marigowda <santosharakere@gmail.com>
2020-04-25 20:30:13 +0100

    a new project and component added to demo magnific popup

---
+++ projects/magnific-popup-demo/src/app/magnific-popup/magnific-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-magnific-popup',
+  templateUrl: './magnific-popup.component.html',
+  styleUrls: ['./magnific-popup.component.scss']
+})
+export class MagnificPopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- projects/slickjs-demo/src/app/app.component.ts
+++ projects/slickjs-demo/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'slickjs-demo';
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    console.log($);
+    // $('.slick-carousel').slick();
+
+    $(document).ready(function () {
+      $('.carousel').slick({
+        slidesToShow: 3,
+        dots: true,
+        centerMode: true,
+      });
+    });
+  }
+
 }
